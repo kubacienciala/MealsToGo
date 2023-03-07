a6081b7e40962bb53ff44cfb646442f9e4394123
kubacienciala <jakubciencialaa@gmail.com>
2023-03-07 19:03:46 +0100

    prettier commit

--- App.js
+++ App.js
@@ -5,27 +5,21 @@ import { theme } from './src/infrastructure/theme';
 
 import {
 	useFonts as useOswald,
-	Oswald_400Regular
-  } from '@expo-google-fonts/oswald';
-import {
-	useFonts as useLato,
-	Lato_400Regular
-  } from '@expo-google-fonts/lato';
-  
-
+	Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
-
 	const [oswaldLoaded] = useOswald({
-		Oswald_400Regular
-	  });
+		Oswald_400Regular,
+	});
 	const [latoLoaded] = useLato({
-		Lato_400Regular
-	  });
+		Lato_400Regular,
+	});
 
-	  if(!oswaldLoaded || !latoLoaded) {
+	if (!oswaldLoaded || !latoLoaded) {
 		return null;
-	  }
+	}
 
 	return (
 		<>
--- src/components/screens/restaurants.js
+++ src/components/screens/restaurants.js
@@ -4,25 +4,24 @@ import { RestaurantInfoCard } from './restaurant-info-card';
 import styled from 'styled-components/native';
 
 const SafeArea = styled(SafeAreaView)`
-flex: 1;
-${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+	flex: 1;
+	${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-padding: ${props => props.theme.space[3]};
+	padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
-flex: 1;
-padding: ${props => props.theme.space[3]};
-background-color: ${props => props.theme.colors.bg.primary};
+	flex: 1;
+	padding: ${(props) => props.theme.space[3]};
+	background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const RestaurantsScreen = () => (
 	<SafeArea>
 		<SearchContainer>
-			<Searchbar
-			/>
+			<Searchbar />
 		</SearchContainer>
 		<RestaurantListContainer>
 			<RestaurantInfoCard />
