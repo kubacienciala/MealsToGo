299f3ac70e4f94d743d5cd07eb039b2d61c82d46
kubacienciala <jakubciencialaa@gmail.com>
2023-03-07 19:01:20 +0100

    Xml images done commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { RestaurantsScreen } from './src/components/screens/restaurants';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from './src/infrastructure/theme';
+
+import {
+	useFonts as useOswald,
+	Oswald_400Regular
+  } from '@expo-google-fonts/oswald';
+import {
+	useFonts as useLato,
+	Lato_400Regular
+  } from '@expo-google-fonts/lato';
+  
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular
+	  });
+	const [latoLoaded] = useLato({
+		Lato_400Regular
+	  });
+
+	  if(!oswaldLoaded || !latoLoaded) {
+		return null;
+	  }
+
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<RestaurantsScreen />
+			</ThemeProvider>
+			<ExpoStatusBar style='auto' />
+		</>
+	);
+}
